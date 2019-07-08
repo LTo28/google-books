b56e8fb33ebcf2ca409618f2699addf3989748d4
LTo28 <to.larry92@gmail.com>
2019-07-07 20:52:57 -0700

    updated server.js

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ if (process.env.NODE_ENV === "production") {
 
 require('./routes')(app)
 
-require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db')
   .then(() => app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`) }))
   .catch(e => console.log(e))
