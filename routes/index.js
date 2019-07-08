cdece36c78422783c9e17e7e7d3e8cbf3721fd69
LTo28 <to.larry92@gmail.com>
2019-07-07 21:46:32 -0700

    updated server

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
-const path = require('path')
-const router = require('express').Router()
-
 module.exports = app => {
   require('./api/bookRoutes.js')(app)
-
-  // If no API routes are hit, send the React app
-  router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -8,10 +10,13 @@ app.use(express.json())
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 require('./routes')(app)
 
-require('mongoose').connect(process.env.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-  .then(() => app.listen(process.env.PORT || 3001, () => { console.log(`Server running on port: ${PORT}`) }))
+require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(() => app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`) }))
   .catch(e => console.log(e))
